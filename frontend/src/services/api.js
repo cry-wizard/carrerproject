56e2a211de682343b62d5c19d6499f40937eed9a
cry-wizard <sidd160306@gmail.com>
2026-03-09 23:05:35 +0530

    Fixing Backend Error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://carrer-page.onrender.com/api"
+  baseURL: "https://carrerproject.onrender.com/api"
 });
 
 export default API;
